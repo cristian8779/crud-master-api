a5653c9aff10775dd436c114e2853475ba72d544
Cristian <cr6145396@gmail.com>
2025-06-29 17:52:52 -0500

    feat: actualiza lógica de esSuperAdmin

--- middlewares/esSuperAdmin.js
+++ middlewares/esSuperAdmin.js
@@ -1,6 +1,6 @@
 const esSuperAdmin = (req, res, next) => {
-  if (req.usuario?.rol !== "superadmin" && req.usuario?.rol !== "root") {
-    return res.status(403).json({ mensaje: "Acceso denegado. Requiere rol superadmin." });
+  if (req.usuario?.rol !== "superAdmin" && req.usuario?.rol !== "root") {
+    return res.status(403).json({ mensaje: "Acceso denegado. Requiere rol superAdmin." });
   }
   next();
 };
